4f3651a99373be3e7ce32ecbb91e48e332ba1d40
saurav <vermasourav120@gmail.com>
2023-05-15 17:30:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(any){
+    document.querySelector('.main-img').src = any;
+}
+function changeCirclecolor(color){
+    const circle=document.querySelector('.circle');
+    circle.style.background = color;
+}
